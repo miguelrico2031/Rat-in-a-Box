81eefe420aef73b5d9ba40895db8b797f1321cdf
Miguel Rico Feo <miguelrico2031@gmail.com>
2024-02-07 00:46:53 +0100

    outline perf

--- Assets/Scripts/Items/Selectable.cs
+++ Assets/Scripts/Items/Selectable.cs
@@ -7,20 +7,25 @@ using UnityEngine.EventSystems;
 public class Selectable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     
-    
+    private Material _spriteMaterial;
+    private int _outlineEnabledID;
     
     private void Awake()
     {
-        
+        _outlineEnabledID = Shader.PropertyToID("_OutlineEnabled");
+        _spriteMaterial = transform.parent.Find("Sprite").GetComponent<SpriteRenderer>().material;
+        _spriteMaterial.SetFloat(_outlineEnabledID, 0f);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        _spriteMaterial.SetFloat(_outlineEnabledID, 1f);
+        Debug.Log("hola");
     }
     
     public void OnPointerExit(PointerEventData eventData)
     {
+        _spriteMaterial.SetFloat(_outlineEnabledID, 0f);
         
     }
     
