bce29c796e30b4bb38a53d2a1a9e19a3937c2b02
Miguel Rico Feo <miguelrico2031@gmail.com>
2024-02-07 00:12:52 +0100

    sprites outline tal

---
+++ Assets/Scripts/Items/Selectable.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selectable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    
+    
+    
+    private void Awake()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+    
+    
+}
