244140ea8ea2517f9dbea958acfc6f471803d64e
m4nu3lgc <loloandthebeastcactus@gmail.com>
2024-02-11 04:05:39 +0100

    Se guarda el volumen entre escenas

--- Assets/Scripts/Sound/Volume.cs
+++ Assets/Scripts/Sound/Volume.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
-    [SerializeField] private Slider musicSlider;
+    public float volumeValue;
+    public Slider slider;
+
+    private void Start()
+    {
+        LoadVolume();
+    }
 
     public void ChangeVolume(float newValue)
     {
         float newVol = AudioListener.volume;
         newVol = newValue;
         AudioListener.volume = newVol;
+        PlayerPrefs.SetFloat("volumeValue", AudioListener.volume);
+        PlayerPrefs.SetFloat("sliderValue", newValue);
+    }
+
+    public void LoadVolume()
+    {
+        volumeValue = PlayerPrefs.GetFloat("volumeValue");
+        slider.value = PlayerPrefs.GetFloat("sliderValue");
     }
 }
