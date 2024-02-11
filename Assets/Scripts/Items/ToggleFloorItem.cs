34b25071ebfc90db008e2bb9aa500da3b30e952a
Borjita <borja.fierro@gmail.com>
2024-02-11 12:32:56 +0100

    puertas terminadas

--- Assets/Scripts/Items/ToggleFloorItem.cs
+++ Assets/Scripts/Items/ToggleFloorItem.cs
@@ -8,12 +8,36 @@ public class ToggleFloorItem : MonoBehaviour
     [SerializeField] private float _waitUntilUpdatePath;
     [SerializeField] private List<ToggleDoor> _doorList = new List<ToggleDoor>();
 
+    [SerializeField] private bool _isActive;
+    [SerializeField] private List<GameObject> _sprites;
+
+    private void Start()
+    {
+        if (!_isActive) return;
+
+        _sprites[0].SetActive(false);
+        _sprites[1].SetActive(true);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         var rat = other.GetComponentInParent<RatController>();
         if (!rat) return;
 
         ToggleDoors();
+        if (_isActive)
+        {
+            _sprites[1].SetActive(false);
+            _sprites[0].SetActive(true);
+            _isActive = false;
+        }
+        else
+        {
+            _sprites[0].SetActive(false);
+            _sprites[1].SetActive(true);
+            _isActive = true;
+        }
+        
     }
 
     private void ToggleDoors()
