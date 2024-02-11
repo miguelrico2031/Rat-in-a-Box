ccde18e68cbbbec5116275e6a45ce01bbcdb7eea
antoonrs <arodriguezseselle@gmail.com>
2024-02-11 11:19:51 +0100

    Ya esta todo JAJSJAJJAS POR FIN

--- Assets/Scripts/Rat/RatController.cs
+++ Assets/Scripts/Rat/RatController.cs
@@ -195,8 +195,14 @@ public class RatController : MonoBehaviour
         //item.Info.InteractAudioName
         switch (item.Info.InteractAudioName)
         {
-            case "gato":
+            case "muere":
                 MusicManager.Instance.PlaySound("muere");
+                break;
+            case "trampa":
+                MusicManager.Instance.PlaySound("trampa");
+                break;
+            case "gato":
+                MusicManager.Instance.PlaySound("rata1");
                 break;
             case "queso":
                 MusicManager.Instance.PlaySound("comeQueso");
@@ -209,6 +215,9 @@ public class RatController : MonoBehaviour
                 break;
             case "peluche":
                 MusicManager.Instance.PlaySound("rata1");
+                break;
+            case "tapa":
+                MusicManager.Instance.PlaySound("tapa");
                 break;
         }
         
